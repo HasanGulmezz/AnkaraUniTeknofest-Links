559d4d4ca39c33b012aaa5b32a59f802710ecc53
Hasan Gulmez <hasangulmez36@gmail.com>
2026-02-05 18:18:55 +0300

    New connection added

--- src/data/content.js
+++ src/data/content.js
@@ -5,29 +5,36 @@ export const profileData = {
 };
 
 export const linksData = [
-  {
+    {
     id: 1,
+    title: "‼️Teknofest Tanıtım Etkinliği‼️",
+    description: "Formu doldurarak Teknofest tanıtım etkinliğimize katılabilirsiniz!",
+    url: "https://docs.google.com/forms/d/1PgVPM4EDz-xLz3i7Beu12jBOZ6jVnuYgnHgqBmXLsCk/edit",
+    image: "/logos/Logo-Son.png", 
+  },
+  {
+    id: 2,
     title: "WhatsApp",
     description: "Whatsapp grubumuza katılmayı unutmayın!",
     url: "https://chat.whatsapp.com/HvawKlrY8Ud7bMJd0KJKBT",
     image: "/logos/WhatsApp.svg.png", 
   },
   {
-    id: 2,
+    id: 3,
     title: "Instagram",
     description: "Instagram sayfamızı takip edin!",
     url: "https://www.instagram.com/teknofestankarauni?igsh=YWtwZXZhMDAzaHY1&utm_source=qr",
     image: "/logos/Instagram.svg.png", 
   },
   {
-    id: 3,
+    id: 4,
     title: "NSosyal",
     description: "NSosyalden bizi takip edin!",
     url: "https://sosyal.teknofest.app/@teknofestankarauni",
     image: "logos/Nsosyal.webp", 
   },
   {
-    id: 4,
+    id: 5,
     title: "Üye Kayıt Formu",
     description: "Üye olmak istiyorsanız burdan formu doldurmayı unutmayın",
     url: "https://docs.google.com/forms/d/1_YkcIRiexhECcV989pwyUYqXLEFW-HE-zhKTNa97UoM",
